a979cc30e4dca9a99ef81c10cde8d51090718459
Kiyokawa Hiromu <hirosan315911@gmail.com>
2024-10-31 12:49:12 +0900

    test.bashにテストを追加

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
         echo ${1}行目が違うよ
@@ -7,8 +7,18 @@ ng () {
 
 res=0
 
+###正しい入力###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
-exit "$res"
+###やばい入力##
+out=$(echo あ | ./plus)
+[ "$?" = "1" ]   || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit $res
