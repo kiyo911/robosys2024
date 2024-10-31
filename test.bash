a2747e05dca5972e82e39a2c7e29c3170171c15e
Kiyokawa Hiromu <hirosan315911@gmail.com>
2024-10-31 12:23:15 +0900

    add p20

--- test.bash
+++ test.bash
@@ -6,8 +6,9 @@ ng () {
 }
 
 res=0
-a=清川
-[ "$a" = 浦河 ] || ng "$LINENO"
-[ "$a" = 清川 ] || ng "LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit "$res"
